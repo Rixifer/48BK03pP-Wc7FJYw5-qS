355372dcc93a021f5091676305362a8ad06b0fbf
Rixifer <jmblackburn21@pm.me>
2026-01-05 15:57:55 -0500

    Local Commit

---
+++ assets/link-etc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm textures-pvr-med
+rm textures-png-med
+ln -s ../../common/generated/textures-etc1-med textures-etc1-med
+ln -s ../../common/generated/textures-etc1png-med textures-etc1png-med
---
+++ assets/link-pvr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm textures-etc1-med
+rm textures-etc1png-med
+ln -s ../../common/generated/textures-pvr-med textures-pvr-med
+ln -s ../../common/generated/textures-png-med textures-png-med
